9c00106a7d2c05e9c14892cbcb89f7385cb3a9a3
Abdullah Al Shafi <68460013+Al-Shafi-Github@users.noreply.github.com>
2023-10-08 14:48:44 +0600

    Updating Controller

--- app/Http/Controllers/OtherController.php
+++ app/Http/Controllers/OtherController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\career;
+
+use App\Models\contacts;
+use App\Models\galleries;
 use App\Models\UserDetails;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
 class OtherController extends Controller
 {
-    public function viewCareerPage(){
+    public function viewCareerPage()
+    {
         return view('/career');
     }
 
@@ -19,45 +23,65 @@ class OtherController extends Controller
             'name' => ['required', 'min:3', 'max:20'],
             'email' => ['required', 'email'],
             'message' => ['required'],
-            'file' => ['required', 'mimes:pdf', 'max:3000'], 
+            'file' => ['required', 'mimes:pdf', 'max:3000'],
         ]);
-    
+
         // Handle file upload and store it
         $file = $request->file('file');
         $fileName = time() . '.' . $file->extension();
         $file->move(public_path('career_files'), $fileName);
-    
+
         // Create a new CareerApplication instance and save it to the database
         $careerApplication = new career;
         $careerApplication->name = $request->name;
         $careerApplication->email = $request->email;
         $careerApplication->message = $request->message;
         $careerApplication->file = $fileName; // Store the file name in the 'resume' column
-    
+
         $careerApplication->save();
-    
+
         return back()->withSuccess('Application Submitted');
     }
 
-    public function blogPage(){
+    public function blogPage()
+    {
         return view('blogpage');
     }
 
-    public function contact() {
+    // contact us related controller
+
+    public function contact()
+    {
         return view('contact_us');
     }
-    public function submitcontact( Request $request) {
+
+    public function submitcontact(Request $request)
+    {
         $request->validate([
             'name' => ['required', 'min:3', 'max:20'],
             'email' => ['required', 'email'],
-             'phone'=> ['required', 'min:11', 'max:14'],
+            'phone' => ['required'],
+            'subject' => ['required'],
             'message' => ['required'],
-         
+
         ]);
 
-        $
+        $contactfiled = new contacts;
+        $contactfiled->name = $request->name;
+        $contactfiled->email = $request->email;
+        $contactfiled->phone = $request->phone;
+        $contactfiled->subject = $request->subject;
+        $contactfiled->message = $request->message;
+
+        $contactfiled->save();
+
 
         return back()->withSuccess('Submitted');
     }
 
+    public function viewGallery()
+    {
+        $users = galleries::all();
+        return view('gallery')->with('users', $users);
+    }
 }
