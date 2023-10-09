0c94be18fb13d8a2a662d8e0e6b368e026883c37
Abdullah Al Shafi <68460013+Al-Shafi-Github@users.noreply.github.com>
2023-10-09 11:09:03 +0600

    Adding Video Carousel

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -17,31 +17,71 @@
             @endforeach
         </section>
         <!-- Section: Images -->
-    </div>
-    <!-- Modal gallery -->
 
-    <style>
-        .bg-image {
-            position: relative;
-            padding-bottom: 100%; /* Maintain a square aspect ratio (1:1) */
-            overflow: hidden;
-        }
+          <!-- Section: Videos -->
+          
+        <div class="container">
+        <div class="mt-5 mb-5">
+            <h1 style="text-align:center;"><strong>Watch Videos</strong></h1>
+        </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-10">
+                    <div id="youtubeCarousel" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <iframe class="d-block w-100" width="800" height="400" src="https://www.youtube.com/embed/KZA_BIa8e7I" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="carousel-item">
+                                <iframe class="d-block w-100" width="800" height="400" src="https://www.youtube.com/embed/nyQWdqWO22w" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="carousel-item">
+                                <iframe class="d-block w-100" width="800" height="400" src="https://www.youtube.com/embed/TyTdnMHZ5Fk" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#youtubeCarousel" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#youtubeCarousel" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-        .image-container {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
 
-        .square-image {
-            object-fit: cover;
-            width: 100%;
-            height: 100%;
-        }
-    </style>
+
+
+        <style>
+            #youtube-player {
+                margin: 0 auto;
+                /* Center the player */
+            }
+
+            .bg-image {
+                position: relative;
+                padding-bottom: 100%;
+                /* Maintain a square aspect ratio (1:1) */
+                overflow: hidden;
+            }
+
+            .image-container {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .square-image {
+                object-fit: cover;
+                width: 100%;
+                height: 100%;
+            }
+        </style>
 </x-layout>
